fa26cb3d4db16c247ac89d88a97bccf8b75f09f9
NummAd <116182616+Nummad-Ahmad@users.noreply.github.com>
2024-11-26 18:16:17 +0500

    Update homePage.js

--- src/homePage.js
+++ src/homePage.js
@@ -1,9 +1,20 @@
 import style from './homePage.module.css';
 import SideBar from './sideBar';
-export default function HomePage(){
+import Dr from './images/dr.png';
+
+import Navbar from './navbar';
+export default function HomePage() {
     return (
         <div className={style.homepage}>
-            <SideBar />
+            {/* <SideBar /> */}
+            <Navbar />
+            <div className={style.imgandtext}>
+                <div className={style.mototaglinediv}>
+                    <p className={style.text}>Your Personalized Companion for Smarter Acne Detection</p>
+                    <p className={style.tagline} >Personalized solutions for every skin type, backed by advanced detection and prevention tools</p>
+                </div>
+                <img className={style.img} src={Dr}></img>
+            </div>
         </div>
     );
 }
