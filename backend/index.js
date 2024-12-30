3c7ba454cdd5f21242b6aaa2f89b8cc1be977b01
NummAd <116182616+Nummad-Ahmad@users.noreply.github.com>
2024-12-30 20:30:38 +0500

    Delete backend directory

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-
-app.listen(8000, () => {
-    console.log('server started');
-});
